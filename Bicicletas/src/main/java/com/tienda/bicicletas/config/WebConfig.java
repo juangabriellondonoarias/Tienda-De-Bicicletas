c974bac6b062be1137140a8e6325945716c1487e
juangabriellondonoarias <juangabriellondonoarias@gmail.com>
2026-02-26 12:31:05 -0500

    Agregacion de config y dto

--- Bicicletas/src/main/java/com/tienda/bicicletas/config/WebConfig.java
+++
@@ -1,8 +0,0 @@
-package com.tienda.bicicletas.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class WebConfig {
-    // Aquí irá la configuración de CORS más adelante
-}
