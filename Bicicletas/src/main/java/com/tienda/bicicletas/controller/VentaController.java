1977c90c2a597a39e6fb1c3cab3554d22474ce1c
Carlos Andres Mnedez <camendez2515@gmail.com>
2026-03-12 14:16:40 -0500

    subeindo creacion tablas venta y detalle venta

--- Bicicletas/src/main/java/com/tienda/bicicletas/controller/VentaController.java
+++ Bicicletas/src/main/java/com/tienda/bicicletas/controller/VentaController.java
@@ -6,12 +6,11 @@ import com.tienda.bicicletas.dto.response.VentaResponseDTO;
 import com.tienda.bicicletas.service.VentaService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("api/ventas")
 public class VentaController {
 
